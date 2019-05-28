bc6b1875a7abb4369f3ed3c0a447bac287b79643
AlphaLy <lyalpha2401@gmail.com>
2019-05-28 11:41:47 -0500

    I added the function to randomly pick a country from the CountryInAfrica array

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var inquirer = require("inquirer");
 var letterArray = "abcdefghijklmnopqrstuvwxyz";
 
 // List of words to choose from
-var CountriesInAfrica = ["algeria", "benin", "cote d'ivoire", "senegal", "mali", "guinee", "morroco", "togo", "mauritania", "congo"];
+var CountriesInAfrica = ["algeria", "benin", "cote divoire", "senegal", "mali", "guinee", "morroco", "togo", "mauritania", "congo"];
 
 // Pick Random index from AfricanCountries array
 var randomIndex = Math.floor(Math.random() * CountriesInAfrica.length);
@@ -22,3 +22,18 @@ var correctLetters = [];
 
 // Guesses left
 var guessesLeft = 10;
+
+function knowledge() {
+
+    // Generates new word for Word constructor if true
+    if (requireNewWord) {
+        // Selects random AfricanCountries array
+        var randomIndex = Math.floor(Math.random() * CountriesInAfrica.length);
+        var randomWord = CountriesInAfrica[randomIndex];
+
+        // Passes random word through the Word constructor
+        computerWord = new Word(randomWord);
+
+        
+        requireNewWord = false;
+    }
